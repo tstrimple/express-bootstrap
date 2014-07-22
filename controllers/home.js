3b6b4662a114f98ef7ba1b8f7ab933d75921d8fd
Timothy Strimple <tim@tstrimple.com>
2014-07-21 19:33:30 -0700

    added navigation section highlight

--- controllers/home.js
+++ controllers/home.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('home/index.jade', { title: 'Express' });
+  res.render('home/index.jade', { key: 'page1', title: 'Express' });
 });
 
 module.exports = router;
